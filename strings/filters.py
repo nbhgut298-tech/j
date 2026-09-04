9cf73b766b3e93f888210d7cbb867cfd951269d3
nbhgut298-tech <nbhgut298@gmail.com>
2026-09-04 15:30:43 +0300

    Add files via upload

---
+++ strings/filters.py
@@ -0,0 +1,15 @@
+# Copyright (c) ShaHm
+
+from typing import List, Union
+
+from pyrogram import filters
+
+
+other_filters = filters.group  & ~filters.via_bot & ~filters.forwarded
+other_filters2 = (
+    filters.private  & ~filters.via_bot & ~filters.forwarded
+)
+
+
+def command(commands: Union[str, List[str]]):
+    return filters.command(commands, "")
